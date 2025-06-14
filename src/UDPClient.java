29b3ac4fc65c4227c43eafb4e5f15f657d75825a
GuoZimo66 <3159168483@qq.com>
2025-06-14 19:26:12 +0800

    update1

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -2,15 +2,10 @@ public class UDPClient
 {
     public static void main(String[] args)
     {
-
-
     }
 
     private static void downloadFile(){}
     private static void sendAndReceive(){}
-
-
-
 }
 
 
