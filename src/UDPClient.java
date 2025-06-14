344928aea954750088de3fe489f3e1fa74c11470
GuoZimo66 <3159168483@qq.com>
2025-06-14 19:24:02 +0800

    origin

---
+++ src/UDPClient.java
@@ -0,0 +1,16 @@
+public class UDPClient
+{
+    public static void main(String[] args)
+    {
+
+
+    }
+
+    private static void downloadFile(){}
+    private static void sendAndReceive(){}
+
+
+
+}
+
+
---
+++ src/UDPServer.java
@@ -0,0 +1,12 @@
+public class UDPServer
+{
+    public static void main(String[] args) {
+
+    }
+    private static void handleFileTransfer() {}
+
+
+
+
+
+}
